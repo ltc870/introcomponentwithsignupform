42bd75b6d7802859674ec0a86d5fe11b8f38e967
Lawrence C <lawrencecaudle870@gmail.com>
2022-11-26 23:24:06 -0500

    corrected base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/pingsinglecolumncomingsoon/",
+    base: "/introcomponentwithsignupform/",
     css: {
         postcss: {
             plugins: [autoprefixer],
