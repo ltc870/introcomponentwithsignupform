0d226e65079cfed0c0f31eea63c6dfd084d45afe
Lawrence C <lawrencecaudle870@gmail.com>
2022-11-26 22:04:52 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Form />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="attribution">
+                Challenge by{" "}
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by{" "}
+                <a href="https://www.lawrencecaudle.com">Lawrence Caudle</a>.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
